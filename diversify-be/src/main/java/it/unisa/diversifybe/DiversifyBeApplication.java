53bae7651e9250d21a12246d4108833c502a9b04
Davide1210 <david.viola63@gmail.com>
2024-11-22 13:39:58 +0100

    Impostazione backend

---
+++ diversify-be/src/main/java/it/unisa/diversifybe/DiversifyBeApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.diversifybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiversifyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiversifyBeApplication.class, args);
+    }
+
+}
