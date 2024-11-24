55f3f22d91b92a07ec3aa54aed7fbe18637993f0
Oleksandr-Kondratiuk-07 <oleksandrk73@gmail.com>
2024-11-24 22:40:02 +0100

    Implementazione bean delle Campagne di CrowdFunding

---
+++ diversify-be/src/main/java/it/unisa/diversifybe/Model/CampagnaCrowdFunding.java
@@ -0,0 +1,35 @@
+package it.unisa.diversifybe.Model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CampagnaCrowdFunding {
+    private String id;
+    private String titolo;
+    private String contenuto;
+    private String email;
+    private List<String> images;
+    private LocalDate data_inizio;
+    private LocalDate data_prevista_fine;
+    private BigDecimal somma_da_raccogliere;
+    private BigDecimal somma_raccolta;
+    private String stato;
+    //ad esempio terminata, attiva, in sospensione
+
+    /* Possibile aggiunta di pagamento PayPal
+    private List<String> paymentMethods;  // Esempio: ["PayPal"]
+
+    public void addPaypalPaymentMethod() {
+        if (!paymentMethods.contains("PayPal")) {
+            paymentMethods.add("PayPal");
+     */
+
+
+}
