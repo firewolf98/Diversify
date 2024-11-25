c4773924fa84110bf15638abf5bf77fa8ca0bcbb
Pasquale <pasquy.anatriello@gmail.com>
2024-11-25 16:15:05 +0100

    implementazione del bean DocumentoInformativo

---
+++ diversify-be/src/main/java/it/unisa/diversifybe/Model/DocumentoInformativo.java
@@ -0,0 +1,13 @@
+package it.unisa.diversifybe.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DocumentoInformativo {
+    private String titolo;
+    private String link;
+}
