b666836d71a511e364271b9742d079db42f4f24e
Pasquale <pasquy.anatriello@gmail.com>
2024-11-26 11:36:18 +0100

    Implementato il bean Benchmark

---
+++ diversify-be/src/main/java/it/unisa/diversifybe/Model/Benchmark.java
@@ -0,0 +1,14 @@
+package it.unisa.diversifybe.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Benchmark {
+    private String tipo;
+    private String gravita;
+    private String descrizione;
+}
