aa1a22cdca6fcd23a76c1beaaf7bd75d1571d9b7
Catello <c.martone9@studenti.unisa.it>
2024-11-27 11:53:04 +0100

    aggiunta notazioni di MongoDB nei Bean

--- diversify-be/src/main/java/it/unisa/diversifybe/Model/Commento.java
+++ diversify-be/src/main/java/it/unisa/diversifybe/Model/Commento.java
@@ -1,12 +1,15 @@
 package it.unisa.diversifybe.Model;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "Commenti")
 public class Commento {
     private String id_autore;
     private String contenuto;
