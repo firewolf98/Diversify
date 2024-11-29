be80bb7dc54c525b481dfe662251b0827325dcda
luf2003 <l.ferrara61@studenti.unisa.it>
2024-11-29 13:30:35 +0100

    implementazione service fe/be della login con Stella Tavolo

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginFormComponent } from "./login-form/login-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginFormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
