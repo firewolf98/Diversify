0597034827a64bb675665e873f61fcbf3a9db85c
Oleksandr-Kondratiuk-07 <oleksandrk73@gmail.com>
2024-12-02 20:51:47 +0100

    Risoluzione conflitti

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './mappa/mappa.component';
- 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MapComponent ],
+  imports: [RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
