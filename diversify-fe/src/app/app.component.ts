c6031de527fa8ab0f1161c4c4f8aac5cae28e976
luf2003 <l.ferrara61@studenti.unisa.it>
2024-12-03 15:23:55 +0100

    Implementazione responsive login e registrazione

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginFormComponent } from "./login-form/login-form.component";
+import { RegistrazioneFormComponent } from "./registrazione-form/registrazione-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginFormComponent, RegistrazioneFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
