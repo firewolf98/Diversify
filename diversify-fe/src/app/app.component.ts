108ef59b0bdb5a106b60ac159c951efa08d30484
Stellal2003 <s.tavolo@studenti.unisa.it>
2024-12-03 15:18:09 +0100

    Implementazione header con media query

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component'; // Percorso corretto al file HeaderComponent
-
 @Component({
   selector: 'app-root',
   standalone: true, // Se utilizzi moduli standalone
-  imports: [RouterOutlet, HeaderComponent], // Aggiungi il componente Header
+  imports: [RouterOutlet], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], // Corretto styleUrls
 })
