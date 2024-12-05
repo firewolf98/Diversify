bef61bd1be9e6b8a6de33279f67a944df98fd6e7
Stellal2003 <s.tavolo@studenti.unisa.it>
2024-12-05 17:12:28 +0100

    Risolto conflitti

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MapComponent } from './mappa/mappa.component';
 
 @Component({
   selector: 'app-root',
