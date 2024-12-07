19bfca3f9da4bc1bb9418f689293afe72a3b27d4
luf2003 <l.ferrara61@studenti.unisa.it>
2024-12-07 12:49:13 +0100

    Implementazione dell'impostazione della homepage

--- diversify-fe/src/app/footer/footer.component.ts
+++ diversify-fe/src/app/footer/footer.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  constructor() {
+    console.log('FooterComponent caricato');
+  }
 }
