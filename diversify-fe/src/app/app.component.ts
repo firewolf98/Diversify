86ea00be46fde8221012595bec46daa48d69df3b
luf2003 <l.ferrara61@studenti.unisa.it>
2024-12-07 11:56:59 +0100

    Risolti conflitti in package-lock.json e app.component.ts durante il merge con develop

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+//devi aggiungere la dipendenza del footer
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], // Corretto: separa i componenti con una virgola
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'], // Corretto: usa `styleUrls` al plurale
+  styleUrls: ['./app.component.css'], 
 })
 export class AppComponent {
   title = 'diversify-fe';
--- diversify-fe/src/app/header/header.component.ts
+++ diversify-fe/src/app/header/header.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor() {
+    console.log('HeaderComponent caricato');
+  }
+}
 
