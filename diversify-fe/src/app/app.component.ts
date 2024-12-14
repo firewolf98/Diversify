68e11e764f2ecabbecb3b00ddc280dfc8b450c5e
Catello <c.martone9@studenti.unisa.it>
2024-12-14 15:52:41 +0100

    rimozione spazi inutili

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
