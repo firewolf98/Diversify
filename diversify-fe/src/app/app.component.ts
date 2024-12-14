e13e9bcd03c0bc5558abceb291588b5743188b0d
GABRIELLA <g.fede@studenti.unisa.it>
2024-12-14 14:38:07 +0100

    implementazione FE della gestione recupero password

--- diversify-fe/src/app/app.component.ts
+++ diversify-fe/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { GestionePasswordFormComponent } from "./gestione-password-form/gestione-password-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GestionePasswordFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], 
 })
